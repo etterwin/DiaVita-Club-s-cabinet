442e6ba096414277fd3346e28f13948d53f7bdcd
Dasha <etterwin.yll@gmail.com>
2017-10-16 12:29:14 +0300

    update

---
+++ assets/js/openHtml.js
@@ -0,0 +1,11 @@
+function openSchool() {
+    document.location.href = "introduction.html";
+}
+
+function openProfile() {
+    document.location.href = "profile.html";
+}
+
+function openTraining() {
+    document.location.href = "training.html";
+}
